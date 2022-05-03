2945675bb15861b106430246ffbd6f0e65a27ab8
Matthew Kauff <matthew.r.kauff@gmail.com>
2022-05-03 16:59:11 -0400

    add group by to look into db

---
+++ group_by_exercises.sql
@@ -0,0 +1,35 @@
+USE employees;
+
+SELECT DISTINCT title
+FROM titles
+GROUP BY title;
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%e' AND last_name LIKE 'e%'
+GROUP BY last_name
+ORDER BY last_name;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%e' AND last_name LIKE 'e%';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+SELECT COUNT(*), last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%'
+GROUP BY last_name
+ORDER BY last_name;
+
+
+SELECT COUNT(*), gender
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender
+ORDER BY gender;
