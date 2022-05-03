44d0c29cc5e128ae255d88c2696ebe2c3f7095aa
Matthew Kauff <matthew.r.kauff@gmail.com>
2022-05-03 11:51:39 -0400

    add where clause practice to the employee db

---
+++ where_exercises.sql
@@ -0,0 +1,38 @@
+USE employees;
+
+SELECT first_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'e%';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%';
+
+SELECT first_name, gender
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya')
+    OR first_name = 'Maya';
+
+SELECT first_name, gender
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+    AND gender = 'male';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'e%'
+    OR last_name Like '%e';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'e%'
+   AND last_name Like '%e';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+    AND last_name NOT LIKE '%qu%';
