eb78b6a598135bd20e74754dc459e1f44c4cb303
Matthew Kauff <matthew.r.kauff@gmail.com>
2022-05-05 15:10:38 -0400

    add quick index exercises to understand the describe, alter, and unique commands

---
+++ indexes_exercises.sql
@@ -0,0 +1,13 @@
+USE employees;
+
+DESCRIBE departments;
+DESCRIBE dept_emp;
+DESCRIBE employees;
+
+USE codeup_test_db;
+
+ALTER TABLE albums
+    ADD UNIQUE (artist, name);
+
+INSERT INTO albums(artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', 1999, 5.5, 'Pop')
